2a9b1584e6076719a7758f4d93f816b69df4ae37
Xavier <xavierjimenezp@gmail.com>
2021-05-04 12:48:22 +0200

    fixed bugs

--- xcluster.py
+++ xcluster.py
@@ -6,8 +6,9 @@ Main script
 """
 
 import argparse
-from functions import *
-from params import *
+from functions import GenerateFiles, MakeData, CNNSegmentation
+import warnings
+import importlib
 
 #------------------------------------------------------------------#
 # # # # # Script # # # # #
@@ -20,34 +21,43 @@ parser.add_argument("-c", "--cluster_catalogs", required=False, type=bool, nargs
 parser.add_argument("-d", "--dataset", required=False, type=bool, nargs="?", const=False)
 parser.add_argument("-t", "--train", required=False, type=bool, nargs="?", const=False)
 parser.add_argument("-p", "--predict", required=False, type=bool, nargs="?", const=False)
+parser.add_argument("-i", "--input", required=False, type=str)
+
 
 args = parser.parse_args()
+
+if args.input is None:
+    import params as p
+    warnings.simplefilter("always")
+    warnings.warn("No parameter file was given, 'params.py' will be used")
+else:
+    p = importlib.import_module(args.input)
 
 if args.nodes is None:
         args.nodes = 1
 
-GenFiles = GenerateFiles(dataset = dataset, bands=bands, output_path = path)
-MData = MakeData(dataset = dataset, bands=bands, planck_path=planck_path, milca_path=milca_path, disk_radius= disk_radius, output_path = path)
-unet = CNNSegmentation(model = model, dataset = dataset, bands=bands, planck_path=planck_path, milca_path=milca_path, epochs=epochs, batch=batch, 
-                       lr=lr, patience=patience, loss=loss, optimizer=optimizer, disk_radius=disk_radius, output_path = path)
+GenFiles = GenerateFiles(dataset = p.dataset, bands=p.bands, output_path = p.path)
+MData = MakeData(dataset = p.dataset, bands=p.bands, planck_path=p.planck_path, milca_path=p.milca_path, disk_radius= p.disk_radius, output_path = p.path)
+unet = CNNSegmentation(model = p.model, dataset = p.dataset, bands=p.bands, planck_path=p.planck_path, milca_path=p.milca_path, epochs=p.epochs, batch=p.batch, 
+                       lr=p.lr, patience=p.patience, loss=p.loss, optimizer=p.optimizer, disk_radius=p.disk_radius, output_path = p.path)
 
 if args.make_directories == True:
 
     GenFiles.clean_temp_directories()
     GenFiles.make_directories()
-    GenFiles.make_directories(output = True, replace=merge_daily_output_directory)
+    GenFiles.make_directories(output = True, replace=p.merge_daily_output_directory)
 
 if args.cluster_catalogs == True:
-    planck_z, planck_no_z, MCXC_no_planck, RedMaPPer_no_planck = MData.create_catalogs(plot=plot_catalogs)
+    planck_z, planck_no_z, MCXC_no_planck, RedMaPPer_no_planck = MData.create_catalogs(plot=p.plot_catalogs)
     # MData.plot_psz2_clusters(planck_path, milca_path, healpix_path)
     # GenFiles.remove_files_from_directory(healpix_path + 'PSZ2/')
 
 if args.dataset == True:
-    MData.train_data_generator(loops=loops, n_jobs=args.nodes, plot=False)
-    MData.preprocess(leastsq=fit_up_to_mode, range_comp=range_compression, plot=plot_dataset)
+    MData.train_data_generator(loops=p.loops, n_jobs=args.nodes, plot=False)
+    MData.preprocess(leastsq=p.fit_up_to_mode, range_comp=p.range_compression, plot=p.plot_dataset)
 
 if args.train == True:
     unet.train_model()
 
 if args.predict == True:
-    unet.evaluate_prediction(plot=plot_prediction, plot_patch = plot_individual_patchs)
+    unet.evaluate_prediction(plot=p.plot_prediction, plot_patch = p.plot_individual_patchs)
