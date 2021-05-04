bd5db23e1470f512386887ca379474f1514eebb0
Xavier <xavierjimenezp@gmail.com>
2021-05-04 11:55:15 +0200

    fixed missing self

--- functions.py
+++ functions.py
@@ -1062,7 +1062,7 @@ class CNNSegmentation(object):
     def train_model(self):
         input_train = np.load(self.dataset_path + 'input_train_pre_f%s_'%self.freq + self.dataset + '.npz')['arr_0']
         input_val = np.load(self.dataset_path + 'input_val_pre_f%s_'%self.freq + self.dataset + '.npz')['arr_0']
-        train_dataset, valid_dataset, test_dataset = self.npy_to_tfdata(batch_size=batch, buffer_size=1000)
+        train_dataset, valid_dataset, test_dataset = self.npy_to_tfdata(batch_size=self.batch, buffer_size=1000)
 
         callbacks = [
             ModelCheckpoint(self.path + "tf_saves/" + self.dataset + "/model_%s_l%s_o%s_f%s_"%(self.model_name, self.loss_name, self.optimizer_name, self.freq) + self.output_name + ".h5", save_best_only=True),
@@ -1439,12 +1439,11 @@ class CNNSegmentation(object):
 
 
     def evaluate_prediction(self, plot=True, plot_patch=True):
-        train_dataset, valid_dataset, test_dataset = self.npy_to_tfdata(batch_size=batch, buffer_size=1000)
+        train_dataset, valid_dataset, test_dataset = self.npy_to_tfdata(batch_size=self.batch, buffer_size=1000)
 
         from tensorflow.keras.utils import CustomObjectScope
 
-        with CustomObjectScope({'iou': losses.iou, 'f1': losses.f1, 'focal_tversky': losses.focal_tversky, 'dsc': losses.dsc,
-                                'tp': losses.tp, 'tn': losses.tn, 'tversky_loss': losses.tversky_loss}):
+        with CustomObjectScope({'iou': losses.iou, 'f1': losses.f1, 'focal_tversky': losses.focal_tversky_loss, 'dsc': losses.dsc, 'tversky_loss': losses.tversky_loss}):
             model = tf.keras.models.load_model(self.path + "tf_saves/" + self.dataset + "/model_%s_l%s_o%s_f%s_"%(self.model_name, self.loss_name, self.optimizer_name, self.freq) + self.output_name + ".h5")
 
         if plot == True:
@@ -1453,7 +1452,7 @@ class CNNSegmentation(object):
                 self.plot_metric(metric)
             pixel_coords = self.show_predictions(model, dataset = test_dataset, num=30, plot=plot_patch)
         else:
-            self.make_predictions(model, dataset, num=30)
+            self.make_predictions(model, dataset = test_dataset, num=30)
             pixel_coords = self.load_predictions()
 
 
@@ -1829,86 +1828,3 @@ class CNNSegmentation(object):
         print('\n')
 
         return len(df_coords), total_objects
-
-
-
-#------------------------------------------------------------------#
-# # # # # Parameters # # # # #
-#------------------------------------------------------------------#
-
-## PATHS
-path = os.getcwd() + '/' 
-planck_path = '/n17data/jimenez/PLANCK/' #path to planck HFI full sky maps
-milca_path = '/n17data/jimenez/PLANCK/' #path to milca full sky map
-healpix_path = path + 'healpix/figures/' #will be created
-
-## GLOBAL
-dataset = 'planck_z'
-bands = ['100GHz','143GHz','217GHz','353GHz']#['100GHz','143GHz','217GHz','353GHz','545GHz','857GHz', 'y-map']
-merge_daily_output_directory = True
-disk_radius = 2.5 #in arcmin, if None distribution between 0 and 15 arcmin
-
-## CATALOGS
-plot_catalogs = False
-
-## DATASET
-loops = 100 #times dataset will be added again to training set with random variations
-fit_up_to_mode = False #if False, MAD will be used instead
-range_compression = True
-plot_dataset = True
-
-## TRAIN
-epochs = 30 #60
-batch = 20
-lr = 1e-2
-patience = 12 #30
-model = 'unet'
-loss = 'tversky_loss'
-optimizer = 'sgd'
-
-## PREDICT
-plot_prediction = True
-plot_individual_patchs = True
-
-#------------------------------------------------------------------#
-# # # # # Script # # # # #
-#------------------------------------------------------------------#
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-n", "--nodes", required=False, type=int, nargs="?", const=1)
-parser.add_argument("-m", "--make_directories", required=False, type=bool, nargs="?", const=False)
-parser.add_argument("-c", "--cluster_catalogs", required=False, type=bool, nargs="?", const=False)
-parser.add_argument("-d", "--dataset", required=False, type=bool, nargs="?", const=False)
-parser.add_argument("-t", "--train", required=False, type=bool, nargs="?", const=False)
-parser.add_argument("-p", "--predict", required=False, type=bool, nargs="?", const=False)
-
-args = parser.parse_args()
-
-if args.nodes is None:
-        args.nodes = 1
-
-GenFiles = GenerateFiles(dataset = dataset, bands=bands, output_path = path)
-MData = MakeData(dataset = dataset, bands=bands, planck_path=planck_path, milca_path=milca_path, disk_radius= disk_radius, output_path = path)
-unet = CNNSegmentation(model = model, dataset = dataset, bands=bands, planck_path=planck_path, milca_path=milca_path, epochs=epochs, batch=batch, 
-                       lr=lr, patience=patience, loss=loss, optimizer=optimizer, disk_radius=disk_radius, output_path = path)
-
-if args.make_directories == True:
-
-    GenFiles.clean_temp_directories()
-    GenFiles.make_directories()
-    GenFiles.make_directories(output = True, replace=merge_daily_output_directory)
-
-if args.cluster_catalogs == True:
-    planck_z, planck_no_z, MCXC_no_planck, RedMaPPer_no_planck = MData.create_catalogs(plot=plot_catalogs)
-    # MData.plot_psz2_clusters(planck_path, milca_path, healpix_path)
-    # GenFiles.remove_files_from_directory(healpix_path + 'PSZ2/')
-
-if args.dataset == True:
-    MData.train_data_generator(loops=loops, n_jobs=args.nodes, plot=False)
-    MData.preprocess(leastsq=fit_up_to_mode, range_comp=range_compression, plot=plot_dataset)
-
-if args.train == True:
-    unet.train_model()
-
-if args.predict == True:
-    unet.evaluate_prediction(plot=plot_prediction, plot_patch = plot_individual_patchs)
